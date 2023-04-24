d96b415c22e7be202207faa5afb085127e5ac370
Vinicius Motta <viniciusmotta8@gmail.com>
2023-04-24 02:01:08 -0300

    feat: change password and validate account

---
+++ app/(auth)/change-password/page.tsx
@@ -0,0 +1,46 @@
+export const metadata = {
+  title: 'Change Password - Trvium',
+  description: 'Change Password',
+}
+
+import Link from 'next/link'
+
+export default function SignUp() {
+  return (
+    <section className="relative">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+
+          {/* Page header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+            <h1 className="h1">Change your password.</h1>
+          </div>
+
+          {/* Form */}
+          <div className="max-w-sm mx-auto">
+            <form>
+              <div className="flex flex-wrap -mx-3 mb-4">
+                <div className="w-full px-3">
+                  <label className="block text-[#666666] text-sm font-medium mb-1" htmlFor="password">Password <span className="text-red-600">*</span></label>
+                  <input id="password" type="password" className="form-input w-full text-[#666666]" placeholder="Password (at least 10 characters)" required />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-4">
+                <div className="w-full px-3">
+                  <label className="block text-[#666666] text-sm font-medium mb-1" htmlFor="password">Confirm Password <span className="text-red-600">*</span></label>
+                  <input id="password" type="password" className="form-input w-full text-[#666666]" placeholder="Password (at least 10 characters)" required />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mt-6">
+                <div className="w-full px-3">
+                  <button className="btn text-white bg-[#5D5DFF] hover:bg-[#38379C] w-full">Update password</button>
+                </div>
+              </div>
+            </form>
+          </div>
+
+        </div>
+      </div>
+    </section>
+  )
+}
---
+++ app/(auth)/validate-email/page.tsx
@@ -0,0 +1,47 @@
+export const metadata = {
+  title: 'Validate Email - Trvium',
+  description: 'Validate Email',
+}
+
+import Link from 'next/link'
+
+export default function ResetPassword() {
+  return (
+    <section className="relative">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+
+          {/* Page header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+            <h1 className="h1 mb-4">Validate your account.</h1>
+            <div className="text-xl text-[#666666]">
+              <span className="text-[#666666]">Enter the code sent to </span>
+              <span className="text-[#666666] font-medium">mail@mail.com</span>
+            </div>
+          </div>
+
+          {/* Form */}
+          <div className="max-w-sm mx-auto">
+            <form>
+              <div className="flex flex-wrap -mx-3 mb-4">
+                <div className="w-full px-3">
+                  <label className="block text-[#666666] text-sm font-medium mb-1" htmlFor="code">Code</label>
+                  <input id="code" type="code" className="form-input w-full text-[#666666]" placeholder="Code received in email" required />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mt-6">
+                <div className="w-full px-3">
+                  <button className="btn text-white bg-[#5D5DFF] hover:bg-[#38379C] w-full">Validate account</button>
+                </div>
+              </div>
+            </form>
+            <div className="text-[#666666] text-center mt-6">
+              Didn’t receive the verification code? <button className="text-[#5D5DFF] hover:text-[#D9D9D9] transition duration-150 ease-in-out">Resend code</button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+  )
+}
