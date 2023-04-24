7024f6f7019c8db2c883b107e4e2e65b584b7216
Vinicius Motta <viniciusmotta8@gmail.com>
2023-04-24 00:29:50 -0300

    fix: border color updated

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -8,7 +8,7 @@ export default function Zigzag() {
   return (
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="py-12 md:py-20 border-t border-gray-800">
+        <div className="py-12 md:py-20 border-t border-[#D9D9D9]">
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
