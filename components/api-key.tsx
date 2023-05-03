37f6a528ad9d5849018ec7f4cca09689e76bc6c5
Vinicius Motta <viniciusmotta8@gmail.com>
2023-05-03 00:03:05 -0300

    style: api key font updated

--- components/api-key.tsx
+++ components/api-key.tsx
@@ -19,7 +19,7 @@ export default function ApiKey() {
                 <circle cx="512" cy="512" r="512" fill="#D9D9D9"/>
                 <path d="M407.9 757.5c-38.8 0-77.6-14.6-107.4-44l-1.4-1.4-.2-.2c-58.6-60.6-56.9-157.6 3.8-216.1 28.6-27.7 66.4-42.9 106.2-42.9 31.2 0 61.2 9.4 86.4 26.8L700.8 274c10.1-10.1 26.5-10.1 36.6 0 10.1 10.1 10.1 26.5 0 36.6l-27.9 27.9 51 51c10.1 10.1 10.1 26.5 0 36.6l-80.8 80.8c-10.1 10.1-26.5 10.1-36.6 0l-51-51-59.8 59.8c42.5 59.3 37.5 142.6-15.5 196.3-29.9 30.3-69.4 45.5-108.9 45.5zM336 675.8l.8.8c39.7 39.2 103.9 38.8 143.1-.9 38.9-39.4 38.8-103-.1-142.2-.2-.2-.4-.4-.7-.6-39.4-38.1-101.2-38.1-140.6 0-40 38.7-41.2 102.8-2.5 142.9zm292.6-256.5 32.7 32.7 44.3-44.3-32.7-32.7-44.3 44.3z" fill="#666666"/>
               </svg>
-              <span className="ml-4 text-2xl hidden sm:inline">API Key</span>
+              <span className="ml-4 text-xl hidden sm:inline">API Key</span>
               <span className="ml-auto mr-2 font-bold hidden sm:inline">{apiKey}</span>
               <button onClick={() => handleCopy()}>
                 <svg className="ml-2 hidden sm:inline" width="36px" height="36px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -31,7 +31,7 @@ export default function ApiKey() {
             {/* Mobile */}
             <div className="sm:hidden mb-2">
               <div>
-                <span className="text-2xl">API Key</span>
+                <span className="text-xl">API Key</span>
               </div>
               <div>
                 <span className="font-bold text-sm">{apiKey}</span>
