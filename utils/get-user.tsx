628f92cbe15bd1bfbe040c4c746c6f5534733f38
Vinicius Motta <viniciusmotta8@gmail.com>
2023-12-03 22:35:18 -0300

    fix: get user method updated

--- utils/get-user.tsx
+++ utils/get-user.tsx
@@ -2,10 +2,15 @@ import axios from "axios";
 import { cookies } from "next/headers";
 
 export const getUser = async () => {
-  const response = await axios.get(`${process.env.AUTH0_BASE_URL}/api/auth/me`, {
-    headers: {
-      cookie: `appSession=${cookies().get('appSession')?.value}`
-    }
-  })
-  return response.data;
+  try {
+    const response = await axios.get(`${process.env.AUTH0_BASE_URL}/api/auth/me`, {
+      headers: {
+        cookie: `appSession=${cookies().get('appSession')?.value}`
+      }
+    })
+    return response.data;
+  } catch (error) {
+    console.log(error)
+    return null
+  }
 }
